a0b74e8572f4aa2714d5ecf2f78979b13b6aa7f0
Minh Tran <minhtran.se@gmail.com>
2020-04-16 21:32:20 +0700

    Updates

---
+++ precache-manifest.0d86957eca584583dd509eeff67fba6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db704bcd71380c2869997c3daa5e5ac",
+    "url": "/react-corona/index.html"
+  },
+  {
+    "revision": "9c59a343d23dbef2c0b7",
+    "url": "/react-corona/static/css/main.4575cf68.chunk.css"
+  },
+  {
+    "revision": "613d1be938cce384a3e8",
+    "url": "/react-corona/static/js/2.d60e1650.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-corona/static/js/2.d60e1650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c59a343d23dbef2c0b7",
+    "url": "/react-corona/static/js/main.7f8469ce.chunk.js"
+  },
+  {
+    "revision": "392f3e4adf0e482a6366",
+    "url": "/react-corona/static/js/runtime-main.4c0eb69c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/react-corona/static/media/corona.d7265326.png"
+  }
+]);
